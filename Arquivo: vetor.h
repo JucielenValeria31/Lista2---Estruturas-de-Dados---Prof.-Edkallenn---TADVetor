ca3f426f4756010305b462df7486370df0563f49
JucielenValeria31 <jucielenvaleriasouza@gmail.com>
2025-06-05 12:43:48 -0300

    Create Arquivo: vetor.h

---
+++ Arquivo: vetor.h
@@ -0,0 +1,25 @@
+/* 
+**    Função : Interface do TAD Vetor de Inteiros
+**    Autor : Jucieélen Valéria Souza / Rafaella Santos do Nascimento
+**    Turma : P2 A
+**    Data  : 07/06/2025
+**    Observações: Definição do TAD e das operações
+*/
+
+#ifndef VETOR_H
+#define VETOR_H
+
+typedef struct {
+    int *dados;
+    int tamanho;
+    int capacidade;
+} Vetor;
+
+Vetor* criarVetor(int capacidade);
+void inserirElemento(Vetor* v, int elemento);
+void removerElemento(Vetor* v, int elemento);
+int consultarElemento(Vetor* v, int posicao);
+void imprimirVetor(Vetor* v);
+void liberarVetor(Vetor* v);
+
+#endif
